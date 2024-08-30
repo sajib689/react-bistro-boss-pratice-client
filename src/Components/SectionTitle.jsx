931c98c7f55eba5d4ba7c1795fabf22cfa803397
sajib689 <jibonraja11@gmail.com>
2024-08-30 13:12:02 +0600

    end design title

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -2,9 +2,10 @@
 
 const SectionTitle = ({heading, subHeading}) => {
     return (
-        <div className="text-center">
-            <h2 className="text-yellow-700">{subHeading}</h2>
-            <h1>{heading}</h1>
+        <div className="text-center flex justify-center flex-col items-center my-8">
+            <h2 className="text-yellow-700 mb-2">{subHeading}</h2>
+           
+            <h1 className="text-3xl uppercase border-y-4 py-4">{heading}</h1>
             
         </div>
     );
